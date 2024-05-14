f17494229d411e2563c05ab55bfaf4a90dc0f3ca
trenoxxious <trenoxxious@gmail.com>
2024-05-14 22:05:08 +0000

    Various updates to design

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 let topLogo = document.getElementById('toplogo');
+let expandMenuButton = document.getElementById('menuexpand');
+let expandedMenu = document.getElementById('expandedmenu');
+let menuOpen = false;
 
 topLogo.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
     behavior: 'smooth'
   });
+});
+
+expandMenuButton.addEventListener('click', () => {
+  if (menuOpen == false) {
+    menuOpen = true;
+    expandedMenu.style.display = 'flex';
+    setTimeout(function () {
+    expandedMenu.style.transform = "scale(100%)";
+    }, 100)
+  } else {
+    menuOpen = false;
+    expandedMenu.style.transform = "scale(0%)";
+    setTimeout(function () {
+      expandedMenu.style.display = 'none';
+    }, 100)
+  }
 });
