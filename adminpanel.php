61f4e70e823989b5afa1fe1a71e6e7a5ce86e463
trenoxxious <trenoxxious@gmail.com>
2024-06-14 16:30:20 +0000

    Update to Admin Dash

--- adminpanel.php
+++ adminpanel.php
@@ -90,11 +90,11 @@ $amount_stmt->close();
         <div class="admin-panel-sec">
             <form method="post" action="scripts/insert_player_cache.php">
                 <label for="playerID">Player ID:</label>
-                <input type="text" id="playerID" name="playerID" required>
+                <input type="text" id="playerID" name="playerID" required><br>
                 <label for="key">Key:</label>
-                <input type="text" id="key" name="key" required>
+                <input type="text" id="key" name="key" required><br>
                 <label for="value">Value:</label>
-                <input type="text" id="value" name="value" required>
+                <input type="text" id="value" name="value" required><br>
                 <input type="hidden" name="type" value="0">
                 <input class="button-main" type="submit" value="Insert Player Cache Data">
             </form>
