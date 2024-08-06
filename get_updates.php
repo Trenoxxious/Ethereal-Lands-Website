20f4f28907fb2bfc9f9b8a09eca6c8a4eca73334
Tristan England <trenoxxious@gmail.com>
2024-08-05 22:59:36 -0400

    Update to Updates

--- get_updates.php
+++ get_updates.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 ini_set('log_errors', 1);
 ini_set('error_log_login', '/home/playethe/public_html/error-login.log');
 
-require '../globals.php';
+require 'globals.php';
 
 // Start the session
 session_start();
